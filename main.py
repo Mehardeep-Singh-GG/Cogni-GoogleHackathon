61472b3b7e3a46c49c461817827b12ef90d76484
Mehardeep Singh <mehardeep.sim@gmail.com>
2024-04-30 23:49:25 +0530

    ne commit

--- main.py
+++ main.py
@@ -19,7 +19,24 @@ def submit_form():
         # For this example, let's just print the submitted data
         print("Learn:", learn)
         print("Implementation:", implementation)
-        return redirect(url_for('index'))  # Redirect to the index page after form submission
+        return render_template('loading.html')  # Redirect to the index page after form submission
+
+@app.route('/form_2')
+def show_form_2():
+    return render_template('form2.html')
+
+@app.route('/submit_form_2', methods=['POST'])
+def submit_form_2():
+    if request.method == 'POST':
+        learn = request.form['learn']
+        implementation = request.form['implementation']
+        # Here, you can process the form data as needed, such as saving to a database, etc.
+        # For this example, let's just print the submitted data
+        print("Learn:", learn)
+        print("Implementation:", implementation)
+        return render_template('loading.html')  # Redirect to the index page after form submission
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
